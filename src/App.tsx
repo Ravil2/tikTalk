f597175f3af5aaa058b074f371da2b413499df8a
Ravil <rafffa141105@gmail.com>
2025-05-16 12:59:42 +0500

    add tailwind

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import ProfileCard from "./components/ProfileCard/ProfileCard"
+
+const App = () => {
+  return (
+    <div>
+      React
+      <ProfileCard />      
+    </div>
+  )
+}
+
+export default App
---
+++ src/components/ProfileCard/ProfileCard.tsx
@@ -0,0 +1,9 @@
+const ProfileCard = () => {
+  return (
+    <div className="text-5xl text-red-300">
+      ProfileCard
+    </div>
+  )
+}
+
+export default ProfileCard
